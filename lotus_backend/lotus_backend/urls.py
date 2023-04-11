5da896dbeee1013c7d5604527cc5e5c81ce19b2a
Eliza <ekoknapp@gmail.com>
2023-04-11 16:37:37 -0400

    cleanup

--- lotus_backend/lotus_backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls')),
-    path("products/", include("products.urls", namespace="products")),
-]
--- lotus_backend/testdb/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import User
-
-# Create your views here.
-
-# look into examples of pushing data
